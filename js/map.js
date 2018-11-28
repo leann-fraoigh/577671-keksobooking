d1d99c988bae2d3660ef8bf6b6b568d137a27487
Leann Fraoigh <d.myshlennikova@gmail.com>
2018-11-28 22:30:33 +0300

    Частично внесены правки

--- js/map.js
+++ js/map.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var OBJECTS_AMOUNT = 8;
+// Данные о жилье
 var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде'];
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var TYPES_EXPLAINED = {'palace': 'Дворец', 'flat': 'Квартира', 'house': 'Дом', 'bungalo': 'Бунгало'};
@@ -14,17 +15,27 @@ var MAP_HEIGTH_MAX = 630;
 var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
 var GUESTS_MAX = 1000;
+// Элементы
+var MAP_ELEMENT = document.querySelector('.map');
+var MAP_PINS_ELEMENT = MAP_ELEMENT.querySelector('.map__pins');
+var MAP_FILTERS_ELEMENT = MAP_ELEMENT.querySelector('.map__filters-container');
+// Шаблоны
+var PIN_TEMPLATE = document.querySelector('#pin')
+.content
+.querySelector('.map__pin');
+var CARD_TEMPLATE = document.querySelector('#card')
+.content
+.querySelector('.map__card');
 
 var arraysToShuffle = {
-  'titlesShuffled': TITLES.slice(),
-  'featuresShuffled': FEATURES.slice(),
-  'photosShuffled': PHOTOS.slice()
+  titlesShuffled: TITLES.slice(),
+  featuresShuffled: FEATURES.slice(),
+  photosShuffled: PHOTOS.slice()
 };
 
-var map = document.querySelector('.map');
-
-var mapUnfade = function () {
-  map.classList.remove('map--faded');
+// Все равно получается не универсально из-за класса map--faded внутри? Но тогда нужно просто делать функцию отключения класса, принимающую на вход элемент и класс?..
+var mapUnfade = function (elementToUnfade) {
+  elementToUnfade.classList.remove('map--faded');
 };
 
 // Создтание раномных чисел.
@@ -48,39 +59,41 @@ var shuffle = function (arr) {
 // Генерация одной карточки
 // Не уверена, что это нужно выделять в отдельную функцию
 var getCard = function (j) {
-  var object = {};
+  var location = {
+    x: getRandomInt(0, (MAP_ELEMENT.clientWidth + 1)),
+    y: getRandomInt(MAP_HEIGTH_MIN, (MAP_HEIGTH_MAX + 1))
+  };
 
-  object.author = {};
-  object.author.avatar = 'img/avatars/user0' + (j + 1) + '.png';
+  var ret = {
+    author: {
+      avatar: 'img/avatars/user0' + (j + 1) + '.png'
+    },
+    location: {
+      x: location.x,
+      y: location.y
+    },
+    offer: {
+      title: arraysToShuffle.titlesShuffled[j],
+      address: location.x + ', ' + location.y,
+      price: getRandomInt(PRICE_MIN, (PRICE_MAX + 1)),
+      type: TYPES[getRandomInt(0, 4)],
+      rooms: getRandomInt(1, (ROOMS_MAX + 1)),
+      guests: getRandomInt(1, (GUESTS_MAX + 1)),
+      checkin: CHECKIN_HOURS[getRandomInt(0, CHECKIN_HOURS.length)],
+      checkout: CHECKOUT_HOURS[getRandomInt(0, CHECKOUT_HOURS.length)],
+      features: arraysToShuffle.featuresShuffled.slice(getRandomInt(0, 6)),
+      description: '',
+      photos: arraysToShuffle.photosShuffled,
+    }
+  };
 
-  object.location = {};
-  // Как тут сделать резиновость, чтобы они оставались привязанными к точкам на карте при изменении размера окна браузера?..
-  // object.location.x = getRandomInt(0, 1200);
-  object.location.x = getRandomInt(0, (map.clientWidth + 1));
-  object.location.y = getRandomInt(MAP_HEIGTH_MIN, (MAP_HEIGTH_MAX + 1));
-
-  object.offer = {};
-  object.offer.title = arraysToShuffle.titlesShuffled[j];
-  // Свойства генерируются по порядку? Отсюда в принципе нельзя сослаться на какое-то из свойств, созданных ниже в коде?
-  object.offer.address = object.location.x + ', ' + object.location.y;
-  object.offer.price = getRandomInt(PRICE_MIN, (PRICE_MAX + 1));
-  object.offer.type = TYPES[getRandomInt(0, 4)];
-  object.offer.rooms = getRandomInt(1, (ROOMS_MAX + 1));
-  object.offer.guests = getRandomInt(1, (GUESTS_MAX + 1));
-  object.offer.checkin = CHECKIN_HOURS[getRandomInt(0, CHECKIN_HOURS.length)];
-  object.offer.checkout = CHECKOUT_HOURS[getRandomInt(0, CHECKOUT_HOURS.length)];
-  object.offer.features = arraysToShuffle.featuresShuffled.slice(getRandomInt(0, arraysToShuffle.featuresShuffled.length));
-  object.offer.description = '';
-  object.offer.photos = arraysToShuffle.photosShuffled;
-
-  return object;
+  return ret;
 };
 
 // Генерация списка карточек
 var getCards = function (toShuffle, cardsAmount) {
 
   for (var key in toShuffle) {
-    // Не очень поняла, зачем тут if и что он проверяет, но иначе проверка ругается
     if (toShuffle.hasOwnProperty(key)) {
       shuffle(toShuffle[key]);
     }
@@ -102,14 +115,10 @@ var renderPins = function () {
 
   // Отрисовка одного пина
   var renderPin = function (card) {
-    var pinTemplate = document.querySelector('#pin')
-    .content
-    .querySelector('.map__pin');
 
-    var pinElement = pinTemplate.cloneNode(true);
-    // поправить с учетом высоты метки!
-    pinElement.style.left = card.location.x + 'px';
-    pinElement.style.top = card.location.y + 'px';
+    var pinElement = PIN_TEMPLATE.cloneNode(true);
+    pinElement.style.left = card.location.x - 25 + 'px';
+    pinElement.style.top = card.location.y - 70 + 'px';
     pinElement.firstElementChild.src = card.author.avatar;
     pinElement.firstElementChild.alt = card.offer.title;
     return pinElement;
@@ -125,48 +134,40 @@ var renderPins = function () {
 };
 
 // Создание элемента с карточкой
-var renderCards = function () {
+var renderCard = function (card) {
   var fragment = document.createDocumentFragment();
 
-  var renderCard = function (card) {
-    var cardTemplate = document.querySelector('#card')
-    .content
-    .querySelector('.map__card');
-
-    var cardElement = cardTemplate.cloneNode(true);
-    cardElement.querySelector('.popup__avatar').src = card.author.avatar;
-    cardElement.querySelector('.popup__title').textContent = card.offer.title;
-    cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
-    cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
-    cardElement.querySelector('.popup__type').textContent = TYPES_EXPLAINED[card.offer.type];
-    // Добавление числа комнат и гостей (склонение только для числа комнат  <= 20)
-    if (card.offer.rooms === 1) {
-      cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комната для ' + card.offer.guests + ' гостей';
-    } else if (card.offer.rooms < 5) {
-      cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
-    } else {
-      cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнат для ' + card.offer.guests + ' гостей';
+  var cardElement = CARD_TEMPLATE.cloneNode(true);
+  cardElement.querySelector('.popup__avatar').src = card.author.avatar;
+  cardElement.querySelector('.popup__title').textContent = card.offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = card.offer.address;
+  cardElement.querySelector('.popup__text--price').textContent = card.offer.price + '₽/ночь';
+  cardElement.querySelector('.popup__type').textContent = TYPES_EXPLAINED[card.offer.type];
+  // Добавление числа комнат и гостей (склонение только для числа комнат  <= 20)
+  if (card.offer.rooms === 1) {
+    cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комната для ' + card.offer.guests + ' гостей';
+  } else if (card.offer.rooms < 5) {
+    cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+  } else {
+    cardElement.querySelector('.popup__text--capacity').textContent = card.offer.rooms + ' комнат для ' + card.offer.guests + ' гостей';
+  }
+  // Конец добавления числа комнат и гостей
+  cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+  cardElement.querySelector('.popup__features').textContent = card.offer.features.join(', ');
+  cardElement.querySelector('.popup__description').textContent = card.offer.description;
+  // Вставка фото
+  cardElement.querySelector('.popup__photo').src = card.offer.photos[0];
+  if (card.offer.photos.length > 1) {
+    for (var i = 1; i < card.offer.photos.length; i++) {
+      var photosBlock = cardElement.querySelector('.popup__photos');
+      var photoElement = cardElement.querySelector('.popup__photo').cloneNode(true);
+      photoElement.src = card.offer.photos[i];
+      photosBlock.appendChild(photoElement);
     }
-    // Конец добавления числа комнат и гостей
-    cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
-    cardElement.querySelector('.popup__features').textContent = card.offer.features.join(', ');
-    cardElement.querySelector('.popup__description').textContent = card.offer.description;
-    // Вставка фото
-    cardElement.querySelector('.popup__photo').src = card.offer.photos[0];
-    if (card.offer.photos.length > 1) {
-      for (var i = 1; i < card.offer.photos.length; i++) {
-        var photosBlock = cardElement.querySelector('.popup__photos');
-        var photoElement = cardElement.querySelector('.popup__photo').cloneNode(true);
-        photoElement.src = card.offer.photos[i];
-        photosBlock.appendChild(photoElement);
-      }
-    }
-    // Конец вставки фото
-    return cardElement;
-  };
+  }
+  // Конец вставки фото
 
-  fragment.appendChild(renderCard(cardsData[0]));
-
+  fragment.appendChild(cardElement);
   return fragment;
 };
 
@@ -174,10 +175,10 @@ var renderCards = function () {
 var cardsData = getCards(arraysToShuffle, OBJECTS_AMOUNT);
 
 // Запускает создание и запись меток
-map.querySelector('.map__pins').appendChild(renderPins());
+MAP_PINS_ELEMENT.appendChild(renderPins());
 
 // Запускает создание и запись карточки
-map.insertBefore(renderCards(), map.querySelector('.map__filters-container'));
+MAP_ELEMENT.insertBefore(renderCard(cardsData[0]), MAP_FILTERS_ELEMENT);
 
 // Показывает карту
-mapUnfade();
+mapUnfade(MAP_ELEMENT);
